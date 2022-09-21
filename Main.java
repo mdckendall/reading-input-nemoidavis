f4c0adcfd4f048b12da64660cf96827b012ba669
nemoi007 <113357061+nemoidavis@users.noreply.github.com>
2022-09-21 17:42:05 +0000

    i had to add a scanner to read my input

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Enter a word:");
-    
+    String str = "Object-oriented"; 
+    Scanner myObj = new Scanner(System.in);
+      System.out.println("Enter a word:");
+    String word = myObj.nextLine();
+   if(str.length()>0){
+     System.out.println("String is not empty and length is:" +str.length() );
+   }
+    str = "";
+    if(str.length()==0){
+      System.out.println("String is empty:" +str.length());
+    }
+  }      
     //Your code will return the length of the entered String
-  }
 }
